9a826fb70c9ff24a1deb8f1fb4adf3aae7f0e3a6
Ahmad Ridhwan <ridhwan@fke.utm.my>
2021-08-26 14:29:26 +0800

    new commit

---
+++ src/main/java/my/utm/App.java
@@ -0,0 +1,14 @@
+package my.utm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("New update.");
+    }
+}
